877d719d82dd1ccf5f2474fae2516ce7fdcd85a1
Kadin Wilson <kadin.t.wilson@gmail.com>
2021-02-06 17:10:01 -0800

    Add README and do a little code cleanup

--- main.js
+++ main.js
@@ -6,20 +6,19 @@ import {level} from './level1_64.js';
 const canvas = document.querySelector('#game');
 const ctx = canvas.getContext('2d');
 
-const V_WIDTH = canvas.width;
-const V_HEIGHT = canvas.height;
 const TILE_SIZE = 64;
-const TILE_WIDTH = 20;
-const TILE_HEIGHT = 12;
-const OFFSET = 40;
+const MAP_WIDTH = 20;
+const MAP_HEIGHT = 12;
+const ATLAS_WIDTH = 27;
+const ATLAS_HEIGHT = 20;
 
-canvas.width = TILE_WIDTH * TILE_SIZE + OFFSET*2;
-canvas.height = TILE_HEIGHT * TILE_SIZE + OFFSET*2;
+canvas.width = MAP_WIDTH * TILE_SIZE;
+canvas.height = MAP_HEIGHT * TILE_SIZE;
 
-let camera = {x: -OFFSET, y: -OFFSET};
-const map = new TileMap(TILE_WIDTH, TILE_HEIGHT, 27, 20, TILE_SIZE, level.indexes);
+const map = new TileMap(MAP_WIDTH, MAP_HEIGHT, 
+                        ATLAS_WIDTH, ATLAS_HEIGHT, TILE_SIZE, level.indexes);
 const world = new World(level.slide, level.bounce);
-let ball = new Ball(100, 375, 25);
+const ball = new Ball(100, 375, 25);
 let lastTime = 0;
 
 // start game on atlas load
@@ -27,7 +26,7 @@ let atlasImg = new Image();
 atlasImg.addEventListener('load', () => {
     // add mouse listener
     canvas.addEventListener('click', (e) => {
-        ball.setWaypoint(e.offsetX + camera.x, e.offsetY + camera.y);
+        ball.setWaypoint(e.offsetX, e.offsetY);
     });
     // start gameloop
     requestAnimationFrame(gameLoop);
@@ -57,7 +56,7 @@ function update(dt) {
 
 function draw() {
     // tiles
-    const tiles = map.getTilesInView(camera.x, camera.y, canvas.width, canvas.height);
+    const tiles = map.getTilesInView(0, 0, canvas.width, canvas.height);
     for (let tile of tiles) 
         ctx.drawImage(atlasImg, tile.atlasX, tile.atlasY, TILE_SIZE, TILE_SIZE,
                                 tile.x, tile.y, TILE_SIZE, TILE_SIZE);
@@ -66,20 +65,14 @@ function draw() {
     let pos = ball.waypoint
     ctx.fillStyle = 'blue';
     ctx.beginPath()
-    ctx.arc(pos.x - camera.x, pos.y - camera.y, ball.radius/3, 0, 7);
+    ctx.arc(pos.x, pos.y, ball.radius/3, 0, 7);
     ctx.fill();
 
     // ball
     pos = ball.position;
     ctx.fillStyle = 'green';
     ctx.beginPath();
-    ctx.arc(pos.x - camera.x, pos.y - camera.y, ball.radius, 0, 7);
+    ctx.arc(pos.x, pos.y, ball.radius, 0, 7);
     ctx.fill();
 }
-
-function moveCamera(position) {
-    camera.x = position.x - V_WIDTH/2;
-    camera.y = position.y - V_HEIGHT/2;
-}
-
 
